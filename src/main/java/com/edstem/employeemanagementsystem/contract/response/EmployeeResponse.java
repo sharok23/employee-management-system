a9de6df02ba95fb18631a24cf99c930bc4dd27d1
Sharok N <Sharukhahamed23@gmail.com>
2024-02-06 11:00:41 +0530

    code refined

--- src/main/java/com/edstem/employeemanagementsystem/contract/response/EmployeeResponse.java
+++ src/main/java/com/edstem/employeemanagementsystem/contract/response/EmployeeResponse.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
